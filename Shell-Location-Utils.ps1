96afd7447cb3a9207d5842274950880744a1f73c
Nathan Miller <nmiller217@gmail.com>
2025-05-11 13:21:40 -0700

    Add scripts for managing shell paths and setting OpenSSH default shell

---
+++ Shell-Location-Utils.ps1
@@ -0,0 +1,25 @@
+# https://superuser.com/a/1528657/901835
+
+###
+$FD = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions'
+(Get-ItemProperty (Get-ChildItem $FD).PSPath).Name
+
+###
+New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT | out-null
+Get-ChildItem 'HKCR:\CLSID' | Where-Object { $_.GetSubkeyNames() -contains 'ShellFolder' } | Select-Object PSChildName, @{N = 'Default'; E = { (Get-ItemProperty $_.PSPath).'(Default)' } }
+
+###
+$Shell = New-Object -ComObject shell.application
+$DT = $Shell.Namespace(0)
+
+Function Unfold ($oFolder) {
+    $oFolder.Items() | Where-Object { ($_.IsFolder -eq $True) -and ($_.Name -notLike 'Fonts') } | ForEach-Object {
+        UnFold $_.GetFolder
+    }
+    $_.GetFolder.Items() | Select-Object Name, Path
+}
+
+$DT.Items() | Where-Object { ($_.IsFolder -eq $True) -and
+    ($_.Name -match 'Control Panel') } | ForEach-Object {
+    Unfold $_.GetFolder
+} | Select-Object name, path -unique | Sort-Object Path | Out-Gridview
--- ssh/SetOpenSshServerDefaultShell.ps1
+++ ssh/SetOpenSshServerDefaultShell.ps1
@@ -41,6 +41,17 @@ function Set-OpenSshServerDefaultShell {
     return $false
 }
 
+# export the current default shell to a reg file for backup purposes
+$regFilePath = "C:\OpenSshServerDefaultShellBackup.reg"
+$regFileContent = @"
+Windows Registry Editor Version 5.00
+
+[HKEY_LOCAL_MACHINE\SOFTWARE\OpenSSH]
+"@DefaultShell"="$currentShell"
+"@
+Set-Content -Path $regFilePath -Value $regFileContent -Force
+
+
 # Have user select a new default shell from a list of options or enter their own.
 $defaultShellOptions = @(
     "I want to enter my own shell path"
