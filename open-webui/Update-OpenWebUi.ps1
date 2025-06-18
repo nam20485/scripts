170e691b3afaae05511dd4a6ffe5f6bfebd2eecb
Nathan Miller <nmiller217@gmail.com>
2025-06-18 03:34:24 -0700

    open webui image utils

---
+++ open-webui/Update-OpenWebUi.ps1
@@ -0,0 +1,3 @@
+docker rm -f open-webui
+docker pull ghcr.io/open-webui/open-webui:main
+docker run -d -p 3000:8080 -v open-webui:/app/backend/data --name open-webui ghcr.io/open-webui/open-webui:main
